71928343a81fc0911f922e4db84aadaeb405fe31
divyang1507 <divhp1507@gmail.com>
2025-10-19 07:52:01 +0530

    solved book section issue

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,13 +1,12 @@
-'use client'
-import BookSection from '@/components/Section/BookSection/BookSection'
-import React from 'react'
+import BookSection from "@/components/Section/BookSection/BookSection";
+import React from "react";
 
 const page = () => {
   return (
-    <div className='flex flex-col items-center justify-center gap-4 p-4'>
-      <BookSection/>
+    <div className="flex flex-col items-center justify-center gap-4 p-4">
+      <BookSection />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
